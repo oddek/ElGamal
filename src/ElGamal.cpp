f2531257b926fcf2ac503b84c879ace17ba42de8
Kent Odde <kentodde89@gmail.com>
2020-10-20 22:38:49 +0200

    Code done, added more to report

--- src/ElGamal.cpp
+++ src/ElGamal.cpp
@@ -4,19 +4,35 @@ using namespace ElGamal;
 
 
 ElGamal::PublicKey::PublicKey(std::string _p, std::string _g, bigInt privKey)
-	 	{
-	 		p = mpz_class(_p, 16);
-	 		q = p - 1;
-	 		g = bigInt(_g, 16);
-	 		h = modExp(g, privKey, p);
-	 	}
+{
+	p = mpz_class(_p, 16);
+	q = p - 1;
+	g = bigInt(_g, 16);
+	h = modExp(g, privKey, p);
+}
 
+//Main encryption function. Takes hex-string
+std::string ElGamal::encrypt(std::string plaintext, PublicKey pubKey)
+{
+	auto blocks = getMessageBlocks(plaintext, bitCount(pubKey.p));
+	std::vector<CipherBlock> cipherBlocks;
 
+	for(auto block : blocks)
+	{
+		cipherBlocks.push_back(encryptBlock(block, pubKey));
+
+	}
+	auto ciphertext = concatCipherBlocks(cipherBlocks, bitCount(pubKey.p));
+	std::cout << "Successfully encrypted " << blocks.size() << " blocks\n";
+	return ciphertext;
+}
+
+//Separate the input string in hex into blocks relative to the size of P, and add PKCS padding scheme to the blocks
 std::vector<bigInt> ElGamal::getMessageBlocks(std::string plaintext, unsigned int pBitSize)
 {
 	std::vector<bigInt> blocks;
 
-	unsigned int blockSize = pBitSize*8 - 11*3;
+	unsigned int blockSize = pBitSize/8 - 11*3;
 
 	for(int i = 0; i < plaintext.size(); i += blockSize)
 	{
@@ -27,8 +43,6 @@ std::vector<bigInt> ElGamal::getMessageBlocks(std::string plaintext, unsigned in
 	}
 	return blocks;
 }
-
-
 
 //PKCS#1V1.5 padding scheme
 bigInt ElGamal::PKCS(std::string message, int nBitLen)
@@ -59,6 +73,97 @@ bigInt ElGamal::PKCS(std::string message, int nBitLen)
 	return padded;
 }
 
+//Performs the ElGamal encryption on a single block < P
+CipherBlock ElGamal::encryptBlock(bigInt m, PublicKey pubKey)
+{
+
+	if(m > pubKey.p)
+	{
+		throw std::invalid_argument( "m larger than p" );
+	}
+	mpz_class y = generateRandomNumber(1, pubKey.q - 1);
+
+	mpz_class s = modExp(pubKey.h, y, pubKey.p);
+
+	mpz_class c1 = modExp(pubKey.g, y, pubKey.p);
+
+	mpz_class c2 = modExp(m*s, 1, pubKey.p);
+
+	//std::cout << "Encrypted block with Y =\n\t" << y << "\n";
+
+	return CipherBlock(c1, c2);
+}
+
+//Pad all C1 and C2 so that the length is uniform, and concatonate them into a single string
+std::string ElGamal::concatCipherBlocks(std::vector<CipherBlock> cipherBlocks, unsigned int pBitSize)
+{
+	std::string ciphertext = "";
+	unsigned int cSize = pBitSize/4;
+	for(auto c : cipherBlocks)
+	{
+		std::string c1 = c.first.get_str(16);		
+		std::string c2 = c.second.get_str(16);
+		while(c1.size() < cSize) c1.insert(0, 1, '0');
+		while(c2.size() < cSize) c2.insert(0, 1, '0');
+
+		ciphertext += c1;
+		ciphertext += c2;
+	}
+
+	return ciphertext;
+}
+
+//Main decryption function
+std::string ElGamal::decrypt(std::string cipherText, PublicKey pubKey, bigInt privKey)
+{
+	//Parse the string and call decryptblock
+	auto cipherBlocks = parseCiphertext(cipherText, bitCount(pubKey.p));
+
+	std::string plaintext = "";
+	for(auto c : cipherBlocks)
+	{
+		bigInt m = decryptBlock(c, pubKey, privKey);
+
+		plaintext += inversePKCS(m, bitCount(pubKey.p));
+	}
+
+	std::cout << "Successfully decrypted " << cipherBlocks.size() << " blocks\n";
+
+	return plaintext;
+}
+
+//Separate the ciphertext into blocks of C1's and C2's
+std::vector<CipherBlock> ElGamal::parseCiphertext(std::string ciphertext, unsigned int pBitSize)
+{
+
+	std::vector<CipherBlock> cipherBlocks;
+
+	unsigned int cipherDigitSize = pBitSize/4;
+
+	for(int i = 0; i < ciphertext.size(); i += cipherDigitSize*2)
+	{
+		auto c1 = ciphertext.substr(i, cipherDigitSize);
+		auto c2 = ciphertext.substr(i+cipherDigitSize, cipherDigitSize);
+
+		cipherBlocks.push_back(CipherBlock(bigInt(c1, 16), bigInt(c2, 16)));
+	}
+
+	return cipherBlocks;
+}
+
+//Decrypt a single block
+mpz_class ElGamal::decryptBlock(CipherBlock c, PublicKey pubKey, bigInt privKey)
+{
+
+	mpz_class s = modExp(c.first, privKey, pubKey.p);
+	mpz_class sInv = modExp(c.first, pubKey.q - privKey, pubKey.p);
+
+	mpz_class m = modExp(c.second * sInv, 1, pubKey.p);
+
+	return m;
+}
+
+//Remove the PKCS padding
 std::string ElGamal::inversePKCS(bigInt input, int nBitLen)
 {
 	std::string data = input.get_str(2);
@@ -88,123 +193,7 @@ std::string ElGamal::inversePKCS(bigInt input, int nBitLen)
 	return output;
 }
 
-std::vector<CipherBlock> ElGamal::parseCiphertext(std::string ciphertext, unsigned int pBitSize)
-{
-
-	std::vector<CipherBlock> cipherBlocks;
-
-	unsigned int cipherDigitSize = pBitSize/4;
-
-	for(int i = 0; i < ciphertext.size(); i += cipherDigitSize*2)
-	{
-		auto c1 = ciphertext.substr(i, cipherDigitSize);
-		auto c2 = ciphertext.substr(i+cipherDigitSize, cipherDigitSize);
-
-		cipherBlocks.push_back(CipherBlock(bigInt(c1, 16), bigInt(c2, 16)));
-	}
-
-	return cipherBlocks;
-}
-
-
-std::string ElGamal::concatCipherBlocks(std::vector<CipherBlock> cipherBlocks, unsigned int pBitSize)
-{
-	std::string ciphertext = "";
-	unsigned int cSize = pBitSize/4;
-	for(auto c : cipherBlocks)
-	{
-		std::string c1 = c.first.get_str(16);		
-		std::string c2 = c.second.get_str(16);
-		while(c1.size() < cSize) c1.insert(0, 1, '0');
-		while(c2.size() < cSize) c2.insert(0, 1, '0');
-
-		ciphertext += c1;
-		ciphertext += c2;
-	}
-
-	return ciphertext;
-}
-
-std::string ElGamal::encrypt(std::string plaintext, PublicKey pubKey)
-{
-
-	auto blocks = getMessageBlocks(plaintext, bitCount(pubKey.p));
-	std::vector<CipherBlock> cipherBlocks;
-
-	for(auto block : blocks)
-	{
-		cipherBlocks.push_back(encryptBlock(block, pubKey));
-
-	}
-
-
-	for(int i = 0; i < cipherBlocks.size(); i++)
-	{
-		std::cout << i << ": ";
-		printCipherBlock(cipherBlocks.at(i));
-
-	}
-
-
-	auto ciphertext = concatCipherBlocks(cipherBlocks, bitCount(pubKey.p));
-
-	return ciphertext;
-}
-
-CipherBlock ElGamal::encryptBlock(bigInt m, PublicKey pubKey)
-{
-
-	if(m > pubKey.p)
-	{
-		throw std::invalid_argument( "m larger than p" );
-	}
-	mpz_class y = generateRandomNumber(1, pubKey.q - 1);
-
-	mpz_class s = modExp(pubKey.h, y, pubKey.p);
-
-	mpz_class c1 = modExp(pubKey.g, y, pubKey.p);
-
-	mpz_class c2 = modExp(m*s, 1, pubKey.p);
-
-	std::cout << "Encrypted block with Y =\n\t" << y << "\n";
-
-	return CipherBlock(c1, c2);
-}
-
-std::string ElGamal::decrypt(std::string cipherText, PublicKey pubKey, bigInt privKey)
-{
-	//Parse the string and call decryptblock
-	auto cipherBlocks = parseCiphertext(cipherText, bitCount(pubKey.p));
-
-	for(int i = 0; i < cipherBlocks.size(); i++)
-	{
-		std::cout << i << ": ";
-		printCipherBlock(cipherBlocks.at(i));
-
-	}
-
-	std::string plaintext = "";
-	for(auto c : cipherBlocks)
-	{
-		bigInt m = decryptBlock(c, pubKey, privKey);
-
-		plaintext += inversePKCS(m, bitCount(pubKey.p));
-	}
-
-	return plaintext;
-}
-
-mpz_class ElGamal::decryptBlock(CipherBlock c, PublicKey pubKey, bigInt privKey)
-{
-
-	mpz_class s = modExp(c.first, privKey, pubKey.p);
-	mpz_class sInv = modExp(c.first, pubKey.q - privKey, pubKey.p);
-
-	mpz_class m = modExp(c.second * sInv, 1, pubKey.p);
-
-	return m;
-}
-
+//Modular exponentiation
 bigInt ElGamal::modExp(bigInt x, bigInt y, bigInt p)
 {
 	bigInt res = 1;
@@ -224,6 +213,7 @@ bigInt ElGamal::modExp(bigInt x, bigInt y, bigInt p)
 	return res;
 }
 
+//Gives a public key struct based on the private key
 PublicKey ElGamal::generatePublicKey(bigInt privKey)
 {
 	PublicKey pubKey
@@ -236,64 +226,13 @@ PublicKey ElGamal::generatePublicKey(bigInt privKey)
 	return pubKey;
 }
 
-
-bigInt ElGamal::generateRandomNumber(int bits)
-{
-	gmp_randclass rr(gmp_randinit_default);
-
-	sleep(1);
-	rr.seed(time(NULL));
-
-	mpz_class ran;
-	ran =rr.get_z_bits(bits);
-	return ran;
-}
-
+//Needed to generate random Y
 bigInt ElGamal::generateRandomNumber(mpz_class min, mpz_class max)
 {
 	return rand() % (max - min + 1) + min;
 }
 
-
-
-
-
-
-
-
-
-void ElGamal::runTestVector1()
-{
-	std::string p = "F1B18AE9F7B4E08FDA9A04832F4E919D89462FD31BF12F92791A93519F75076D6CE3942689CDFF2F344CAFF0F82D01864F69F3AECF566C774CBACF728B81A227";
-	std::string g = "07";
-	std::string y = "688628C676E4F05D630E1BE39D0066178CA7AA83836B645DE5ADD359B4825A12B02EF4252E4E6FA9BEC1DB0BE90F6D7C8629CABB6E531F472B2664868156E20C";
-	std::string x = "14E60B1BDFD33436C0DA8A22FDC14A2CCDBBED0627CE68";
-	std::string k = "38DBF14E1F319BDA9BAB33EEEADCAF6B2EA5250577ACE7";
-	std::string pt = "48656C6C6F207468657265";
-	std::string ct1 = "290F8530C2CC312EC46178724F196F308AD4C523CEABB001FACB0506BFED676083FE0F27AC688B5C749AB3CB8A80CD6F7094DBA421FB19442F5A413E06A9772B";
-	std:: string ct2 = "1D69AAAD1DC50493FB1B8E8721D621D683F3BF1321BE21BC4A43E11B40C9D4D9C80DE3AAC2AB60D31782B16B61112E68220889D53C4C3136EE6F6CE61F8A23A0";
-}
-
-void ElGamal::runTestVector2()
-{
-	std::string p = "BA4CAEAAED8CBE952AFD2126C63EB3B345D65C2A0A73D2A3AD4138B6D09BD933";
-	std::string g = "05";
-	std::string y = "60D063600ECED7C7C55146020E7A31C4476E9793BEAED420FEC9E77604CAE4EF";
-	std::string x = "1D391BA2EE3C37FE1BA175A69B2C73A11238AD77675932";
-	std::string k = "F5893C5BAB4131264066F57AB3D8AD89E391A0B68A68A1";
-	std::string pt = "48656C6C6F207468657265";
-	std::string ct1 = "32BFD5F487966CEA9E9356715788C491EC515E4ED48B58F0F00971E93AAA5EC7";
-	std:: string ct2 = "7BE8FBFF317C93E82FCEF9BD515284BA506603FEA25D01C0CB874A31F315EE68";
-}
-//https://chromium.googlesource.com/external/github.com/dlitz/pycrypto/+/2.6-winbuild2-wip/lib/Crypto/SelfTest/PublicKey/test_ElGamal.py
-void ElGamal::runTestVectors()
-{
-	runTestVector1();
-	runTestVector2();
-}
-
-
-
+//Helper
 std::string ElGamal::plaintextToHexString(std::string plaintext)
 {
 	std::stringstream stream;
@@ -307,6 +246,7 @@ std::string ElGamal::plaintextToHexString(std::string plaintext)
 	return res;
 }
 
+//Helper
 std::string ElGamal::hexStringToPlaintext(std::string s)
 {
 	std::string plain = "";
@@ -318,9 +258,7 @@ std::string ElGamal::hexStringToPlaintext(std::string s)
 	return plain;
 }
 
-
-
-
+//Helper
 uintmax_t ElGamal::bitCount(mpz_class n)
 {
 	uintmax_t count = 0;
@@ -332,11 +270,11 @@ uintmax_t ElGamal::bitCount(mpz_class n)
 	return count;
 }
 
+//Printing
 void ElGamal::printNumberDetails(std::string name, bigInt a)
 {
 	std::cout << "\t" << name << ":\n\t\tSize in bits:\t" << bitCount(a) << "\n\t\tNum of digits:\t" << a.get_str().size() << "\n\n\t\t" << a << "\n\n";
 }
-
 void ElGamal::printParameters(PublicKey pubKey, bigInt privKey)
 {
 	std::cout << "Parameters:\n";
@@ -344,9 +282,7 @@ void ElGamal::printParameters(PublicKey pubKey, bigInt privKey)
 	printNumberDetails("q", pubKey.q);
 	printNumberDetails("g", pubKey.g);
 	printNumberDetails("x", privKey);
-
 }
-
 void ElGamal::printCipherBlock(CipherBlock c)
 {
 	std::cout << "CipherBlock: \n";
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,47 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <math.h> 
-#include <gmp.h>
-#include <unistd.h>
-#include <algorithm>
-#include <gmpxx.h>
-#include <bitset>
-#include <sstream>
-#include <random>
-#include <chrono>
+#include <fstream>
 #include "ElGamal.h"
 
-
+std::string readFile(std::string fileName);
+void writeFile(std::string fileName, std::string contents);
 
 int main()
 {
+	std::string inputFile = "test.txt";
+	std::string encryptFile = "encrypted.txt";
+	std::string decryptFile = "decrypted.txt";
+
+
 	srand(time(0));
-	
 	try
 	{
-		std::string message = "jfkd";//kdljfkdahfkdajfdlkajfkdlajkld";
-		std::cout << "Message to be encrypted:\n\t" << message << "\n";
 		bigInt privKey = 4;
-		
 		auto pubKey = ElGamal::generatePublicKey(privKey);
-
 		ElGamal::printParameters(pubKey, privKey);
 
-
+		std::cout << "Reading message from " << inputFile << "\n";
+		std::string message = readFile(inputFile);//kdljfkdahfkdajfdlkajfkdlajkld";
+		std::cout << "Message to be encrypted is " << message.size() << " characters long\n\n";
+		std::cout << "Encrypting message..\n";
 		auto ciphertext = ElGamal::encrypt(ElGamal::plaintextToHexString(message), pubKey);
-		std::cout << "Ciphertext: \n\t" << ciphertext << "\n";
-		
-		
-		auto plaintext = ElGamal::decrypt(ciphertext, pubKey, privKey);
-		std::cout << "Decrypted:\n\t" << ElGamal::hexStringToPlaintext(plaintext) << "\n";
+		writeFile(encryptFile,  ciphertext);
+		std::cout << "Wrote encrypted message to " << encryptFile << "\n";
+		std::cout << "Encrypted message is " << ciphertext.size() << " hex characters long\n\n";
 
+		std::cout << "Decrypting message..\n"; 
+		auto plaintext = ElGamal::hexStringToPlaintext(ElGamal::decrypt(ciphertext, pubKey, privKey));
+		writeFile(decryptFile, plaintext);
+		std::cout << "Wrote decrypted message to " << decryptFile << "\n";
+		std::cout << "Decrypted message was " << plaintext.size() << " characters long\n\n";
+
+		if(message == plaintext)
+		{
+			std::cout << "Decrypted message is equal to input\n";
+		}
+		else
+		{
+			std::cout << "Decrypted message is NOT equal to input \n";
+		}
 
 	}
 	catch (std::exception& e)
@@ -46,6 +52,20 @@ int main()
 
 }
 
+std::string readFile(std::string fileName)
+{
+	std::fstream file;
+	file.open(fileName);
+	std::stringstream sstream;
+	sstream << file.rdbuf();
+	file.close();
+	return sstream.str();
+}
 
-
-
+void writeFile(std::string fileName, std::string contents)
+{
+	std::ofstream file;
+	file.open(fileName);
+	file << contents;
+	file.close();
+}
